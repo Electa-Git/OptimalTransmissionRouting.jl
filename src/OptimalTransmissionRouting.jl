46b0a4dcaed6989c263059bd841608a02669c9a2
Hakan Ergun <hakan.ergun@ieee.org>
2021-07-14 10:13:20 +0200

    fix folder structure

--- src/OptimalTransmissionRouting.jl
+++ src/OptimalTransmissionRouting.jl
@@ -14,19 +14,19 @@ const _LOGGER = Memento.getlogger(@__MODULE__)
 # NOTE: If this line is not included then the precompiled `_PM._LOGGER` won't be registered at runtime.
 __init__() = Memento.register(_LOGGER)
 
-include("src/define_weights.jl")
+include("core/define_weights.jl")
 
 
 # Spatial image files
-include("src/spatial_image_files/clc_agricultural.tiff")
-include("src/spatial_image_files/clc_mountains.tiff")
-include("src/spatial_image_files/clc_natural.tiff")
-include("src/spatial_image_files/clc_urban.tiff")
-include("src/spatial_image_files/clc_graph_Europe.tiff")
-include("src/spatial_image_files/clc_grid_225kv_400kv.tiff")
-include("src/spatial_image_files/natura2000.tiff")
-include("src/spatial_image_files/clc_railroads.tiff")
-include("src/spatial_image_files/clc_roads.tiff")
-include("src/spatial_image_files/sea.tiff")
+include("spatial_image_files/clc_agricultural.tiff")
+include("spatial_image_files/clc_mountains.tiff")
+include("spatial_image_files/clc_natural.tiff")
+include("spatial_image_files/clc_urban.tiff")
+include("spatial_image_files/clc_graph_Europe.tiff")
+include("spatial_image_files/clc_grid_225kv_400kv.tiff")
+include("spatial_image_files/natura2000.tiff")
+include("spatial_image_files/clc_railroads.tiff")
+include("spatial_image_files/clc_roads.tiff")
+include("spatial_image_files/sea.tiff")
 
 end
