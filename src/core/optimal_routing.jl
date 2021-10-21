58ac6f7d42e3752c8a4b15e794c2f678d0184dd5
Hakan Ergun <hakan.ergun@ieee.org>
2021-10-21 11:37:39 +0200

    Try deploy docs

--- src/core/optimal_routing.jl
+++
@@ -1,6 +0,0 @@
-function do_optimal_routing(input_data)
-    
-    spatial_data, spatial_data_matrices, cost_data, equipment_data = prepare_spatial_data(input_data)
-    c_tot, optimal_path, ac_dc, ac_cab, dc_cab = optimize_route(spatial_data, spatial_data_matrices, cost_data, equipment_data, input_data)
-    return spatial_data, spatial_data_matrices, cost_data, equipment_data, c_tot, optimal_path, ac_dc, ac_cab, dc_cab 
-end
